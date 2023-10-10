80898a7bb9b59172561f3936dbf9b44dff23d2d1
Diego Pazzini <diegopazzini2009@hotmail.com>
2023-10-10 01:15:29 -0300

    First Commit

---
+++ PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package DiegoPazzini;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("HelloWorld");
+		}
+	}
+
